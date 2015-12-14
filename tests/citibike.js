a1363c545c3374cee23fab329a3c0a52ed5f647e
Will Washburn <will.washburn@gmail.com>
2015-12-14 15:45:13 -0500

    Find the first station under 100 meters from lat/lng
    cc @khaliqgant

--- tests/citibike.js
+++ tests/citibike.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var citibike = require('../lib/citibike')
 
 
-test('search citibike station test', function (t) {
+test('search citibike station test: empty', function (t) {
 
     t.plan(1);
 
@@ -16,7 +16,7 @@ test('search citibike station test', function (t) {
 
 });
 
-test('search citibike station test', function (t) {
+test('search citibike station test: 5th and 29th', function (t) {
 
     t.plan(1);
 
@@ -31,8 +31,23 @@ test('search citibike station test', function (t) {
 });
 
 
+test('search citibike station test: reverse! 29th and 5th', function (t) {
 
-test('search citibike station test', function (t) {
+    t.plan(1);
+
+    var station = '29th and 5th';
+
+    citibike.searchForStation(station, function(data){
+        t.equal(474, data.number);
+    },function(){
+        console.log('Failed. Sucks.');
+    });
+
+});
+
+
+
+test('search citibike station test: broadway and 29th', function (t) {
 
     t.plan(1);
 
@@ -47,7 +62,7 @@ test('search citibike station test', function (t) {
 });
 
 
-test('search citibike station test', function (t) {
+test('citibike locaition search: broadway and 22nd', function (t) {
 
     t.plan(1);
 
